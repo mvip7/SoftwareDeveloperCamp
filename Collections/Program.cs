97f48a044cd0c65e40772f9945288f0e5c18b35e
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2022-10-30 11:40:26 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> isimler = new List<string> { "Yunus", "Mark", "Alla", "Amırğ" };
+            isimler.Add("Sofiya");
+            for (int i = 0; i < isimler.Count; i++)
+            {
+                Console.WriteLine(isimler[i].ToString());
+            }
+        }
+    }
+}
+
---
+++ EnginDemirog/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.ProductID = 1;
+            product.CategoryID =2;
+            product.ProductName = "Buzdolabı";
+            product.UnitPrice = 4800;
+            product.UnitStocks = 98;
+
+            ProductService productService = new ProductService();
+            productService.Add(product);
+            Console.WriteLine(product.ProductName);
+
+            ProductService productService1 = new ProductService();
+
+            int sonu = productService.Topla(2, 6);
+
+
+        }
+    }
+}
