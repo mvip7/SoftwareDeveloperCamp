6e9aedcc902cc58bb5b44fd35bbf4900f4afedc4
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2022-10-30 22:01:31 +0300

    Class Metot Demo

---
+++ Examples/3-3/ClassMetotDemo/CustomersManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+     class CustomersManager
+    {
+        public void Add(Customers customers)
+        {
+            Console.WriteLine(customers.CustomerName + " " + customers.CustomerLastName + " İsimli Müşteri Eklendi.");
+        }
+        public void Update(Customers customers)
+        {
+            Console.WriteLine(customers.CustomerName + " " + customers.CustomerLastName + " İsimli Müşteri Güncellendi.");
+        }
+        public void Delete(Customers customers)
+        {
+            Console.WriteLine(customers.CustomerName + " " + customers.CustomerLastName + " Müşteri Silindi.");
+        }
+        public void List(Customers[] customers)
+        {
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item.CustomerID.ToString() + " " + item.CustomerName + " " + item.CustomerLastName + " " + item.City + " " + item.CustomerSalary.ToString()); ;
+            }
+        }
+
+    }
+}
