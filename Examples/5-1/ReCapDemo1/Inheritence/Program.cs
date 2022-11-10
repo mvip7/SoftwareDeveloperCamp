c91ff8b4a30926a2332b757e6aee4c3670a9cbce
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2022-11-10 20:52:53 +0300

    10.11.22

---
+++ Examples/5-1/ReCapDemo1/Inheritence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mevzuat mevzuat = new Mevzuat();
+        }
+    }
+    abstract class Mevzuat
+    {
+        public Mevzuat()
+        {
+            Console.WriteLine("Ctor Yapısı");
+        }
+    }
+    class Person
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+    class Customer : Person
+    {
+
+    }
+
+}
---
+++ Examples/5-1/ReCapDemo1/Interfaces/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        interface IWorker
+        {
+            void Work();
+        }
+        interface IEat
+        {
+            void Eat();
+        }
+        interface ISalary
+        {
+            void GetSalary();
+        }
+        class Manager : IWorker, IEat, ISalary
+        {
+            public void Eat()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void GetSalary()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Work()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class RobotsWorker : IWorker
+        {
+            public void Work()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
