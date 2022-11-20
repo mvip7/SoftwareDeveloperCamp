d078e3dfd61bfb61fb82d9a55e5d0f3c586be28f
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2022-11-20 20:06:44 +0300

    20.11.22

---
+++ Examples/8-2/EntityFrameworkDemo/EntityFrameworkDemo/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public int CategoryID { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
---
+++ Examples/8-2/EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAll();
+            GetAllByCategoryID(6);
+        }
+
+        private static void GetAllByCategoryID(int categoryID)
+        {
+            NorthwindContext context = new NorthwindContext();
+            var result = context.Products.Where(p => p.CategoryID == categoryID);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            foreach (var item in context.Products)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
