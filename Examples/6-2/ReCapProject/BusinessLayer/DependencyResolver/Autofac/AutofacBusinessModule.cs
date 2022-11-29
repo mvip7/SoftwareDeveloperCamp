815a6e369731733c0a4f8d71a2dccc46aa6e7ddc
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2022-11-29 13:40:11 +0300

    29.11.22

---
+++ Examples/6-2/ReCapProject/BusinessLayer/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.DependencyResolver.Autofac
+{
+    public class AutofacBusinessModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+        }
+    }
+}
--- Examples/6-2/ReCapProject/WebAPI/Program.cs
+++ Examples/6-2/ReCapProject/WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using BusinessLayer.Abstract;
 using BusinessLayer.Concrete;
+using BusinessLayer.DependencyResolver.Autofac;
 using DataAccessLayer.Abstract;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -17,10 +20,15 @@ namespace WebAPI
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            
+
         }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinessModule());
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
