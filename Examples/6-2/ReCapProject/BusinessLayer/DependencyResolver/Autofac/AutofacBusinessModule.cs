6b0ea6157814f5c013ee4dc72fb01fefe56352c4
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2022-11-29 13:47:37 +0300

    29.11.22

--- Examples/6-2/ReCapProject/BusinessLayer/DependencyResolver/Autofac/AutofacBusinessModule.cs
+++ Examples/6-2/ReCapProject/BusinessLayer/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -13,8 +13,25 @@ namespace BusinessLayer.DependencyResolver.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+
             builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
             builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+
+            builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
+            builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
+
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+
+            builder.RegisterType<RentalsInfoManager>().As<IRentalsInfoService>().SingleInstance();
+            builder.RegisterType<EfRentalsInfoDal>().As<IRentalsInfoDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+
+
         }
     }
 }
